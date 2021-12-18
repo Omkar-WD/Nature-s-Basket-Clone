580cd4408b90a5fcff1466280aa7fd13e28c28f8
Omkar Pasalkar <omkarpasalkar2020@gmail.com>
2021-12-18 21:34:03 +0530

    merge login and signup page functionality to landing page

--- SignUp (K)/signUp.js
+++ SignUp (K)/signUp.js
@@ -3,44 +3,46 @@ var userDataBase = JSON.parse(localStorage.getItem("userDataBase")) || [];
 var flag = false;
 
 function signUp(event) {
-    event.preventDefault();
-    var email = document.querySelector("#email").value;
-    var number = document.querySelector("#number").value;
-    var password = document.querySelector("#password").value;
-
-    var userData = {
-        userEmail: email,
-        userNumber: number,
-        userPassword: password
+  event.preventDefault();
+  var username = document.querySelector("#username").value;
+  var email = document.querySelector("#email").value;
+  var number = document.querySelector("#number").value;
+  var password = document.querySelector("#password").value;
+  localStorage.setItem("userName", username);
+  var userData = {
+    userName: username,
+    userEmail: email,
+    userNumber: number,
+    userPassword: password,
+  };
+  if (userDataBase.length != 0) {
+    var arr = JSON.parse(localStorage.getItem("userDataBase")) || [];
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i].userEmail == email || arr[i].userNumber == number) {
+        flag = true;
+        break;
+      }
     }
-    if (userDataBase.length != 0) {
-        var arr = JSON.parse(localStorage.getItem("userDataBase"));
-        for (var i = 0; i < arr.length; i++) {
-            if (((arr[i].userEmail == email) || (arr[i].userNumber == number))) {
-                flag = true;
-                break;
-            }
-        }
-        if (flag == true) {
-            alert("user already exists!!!");
-        }
-        else {
-            userDataBase.push(userData);
-            localStorage.setItem("userDataBase", JSON.stringify(userDataBase));
-            alert("Signup Successful!!!");
-            window.location.href = "../Login (K)/login.html";
-            document.querySelector("#email").value = "";
-            document.querySelector("#number").value = "";
-            document.querySelector("#password").value = "";
-        }
+    if (flag == true) {
+      alert("user already exists!!!");
+    } else {
+      userDataBase.push(userData);
+      localStorage.setItem("userDataBase", JSON.stringify(userDataBase));
+      alert("Signup Successful!!!");
+      window.location.href = "../Login (K)/login.html";
+      document.querySelector("#username").value = "";
+      document.querySelector("#email").value = "";
+      document.querySelector("#number").value = "";
+      document.querySelector("#password").value = "";
     }
-    else {
-        userDataBase.push(userData);
-        localStorage.setItem("userDataBase", JSON.stringify(userDataBase));
-        alert("Signup Successful!!!");
-        window.location.href = "../Login (K)/login.html";
-        document.querySelector("#email").value = "";
-        document.querySelector("#number").value = "";
-        document.querySelector("#password").value = "";
-    }
+  } else {
+    userDataBase.push(userData);
+    localStorage.setItem("userDataBase", JSON.stringify(userDataBase));
+    alert("Signup Successful!!!");
+    window.location.href = "../Login (K)/login.html";
+    document.querySelector("#username").value = "";
+    document.querySelector("#email").value = "";
+    document.querySelector("#number").value = "";
+    document.querySelector("#password").value = "";
+  }
 }

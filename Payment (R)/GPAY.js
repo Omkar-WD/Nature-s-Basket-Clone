4c69aade4376123666037091281c534f648e8af0
Omkar Pasalkar <omkarpasalkar2020@gmail.com>
2021-12-18 13:23:57 +0530

    created payment section

---
+++ Payment (R)/GPAY.js
@@ -0,0 +1,28 @@
+
+ document.querySelector(".btn").addEventListener("click", proceed);
+ // document.querySelector("#newAccount").addEventListener("click", signup);
+ var userData = JSON.parse(localStorage.getItem("userDatabase")) || [];
+ // console.log(regdUsers);
+ // function signup() {
+ // window.location.href = "signup.html";
+ //}
+ function proceed() {
+   
+   var cardname = document.querySelector("#cardname").value;
+
+   var userCred = {
+    
+     cardname: cardname,
+   };
+
+   userData.push(userCred);
+   localStorage.setItem("userDatabase", JSON.stringify(userData));
+
+   if ( cardname == "abcd@gpay") {
+     //window.location.href = "admin.html";
+     console.log("s");
+     alert("payment sucessful");
+   } else {
+     alert("invalid GPay id");
+   }
+ }

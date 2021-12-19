a509bb0dfc17ffb73702883621d5922423add6f3
Omkar Pasalkar <omkarpasalkar2020@gmail.com>
2021-12-19 12:26:11 +0530

    updated orderId part in all pages

--- Payment (R)/GPAY.js
+++ Payment (R)/GPAY.js
@@ -16,8 +16,8 @@ function proceed() {
 document.querySelector("#amount").textContent =
   localStorage.getItem("cartAmount");
 
-document.querySelector("#orderIdDisp").textContent =
-  "ORD00000" + localStorage.getItem("orderId");
+var number = Number(localStorage.getItem("orderId")) + 1;
+document.querySelector("#orderIdDisp").textContent = "ORD00000" + number;
 
 document.querySelector("#back-button").addEventListener("click", function () {
   window.location.href = "newupi.html";
